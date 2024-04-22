d545373f51cfaaa758a4b836a8f5c453e4bfb557
ViktoriaChuprina <162799082+ViktoriaChuprina@users.noreply.github.com>
2024-04-22 22:14:02 +0300

    Add files via upload

--- script-1.js
+++ script-1.js
@@ -1,2 +1,18 @@
-let submit_btn = document.querySelector('.submit')
-submit_btn.innerHTML = 'Відправити'
+class User{
+    constructor(name, surname, email, password, date, phone, gender){
+        this.name = 'name'
+        this.surname = 'surname'
+        this.email = 'email'
+        this.password = 'password'
+        this.date = 'date'
+        this.phone = 'phone'
+        this.gender = 'gender'
+    }
+}
+let user = new UserActivation('Вікторія', 'Чуприна', 'viktoriachuprina07@gmail.com', 'Aasdfghjkl2007', '19.04.07', '+3800674233031', 'female')
+
+$("#signup").click(function() {
+    if ($(".form-class").hasClass("submit-btn")) {
+      $(".form-class").removeClass("submit-btn");
+    }
+  }  );
